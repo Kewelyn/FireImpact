1d758dfd76f424374f9849a76c366d3112a03ee6
Kewelyn <kewelyncout@gmail.com>
2024-06-12 17:13:28 -0300

    atualizando as urls

--- projeto/app/urls.py
+++ projeto/app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from .viewsets import BiomaAmazoniaViewSet, BiomaCerradoViewSet, BiomaCaatingaViewSet, BiomaPampaViewSet, BiomaPantanalViewSet, BiomaMataAtlanticaViewSet
-from .views import home, listar_amazonia, readBiomaCerrado, readBiomaCaatinga, readBiomaPampa, readBiomaPantanal, readBiomaMataAtlantica, delete_amazonia, deleteBiomaCerrado, deleteBiomaCaatinga, deleteBiomaPampa, deleteBiomaPantanal, deleteBiomaMataAtlantica
+from .views import home, listar_amazonia, listar_cerrado, listar_caatinga, listar_pampa, listar_pantanal, listar_atlantica, delete_amazonia, delete_cerrado, delete_caatinga, delete_pampa, delete_pantanal, delete_atlantica
 
 router = routers.DefaultRouter()
 router.register(r'biomaamazonia', BiomaAmazoniaViewSet, basename="BiomaAmazonia")
@@ -15,16 +15,16 @@ router.register(r'biomamataatlantica', BiomaMataAtlanticaViewSet, basename="Biom
 urlpatterns = [
     path('', home, name='home'),
     path('listar_amazonia', listar_amazonia, name='listar_amazonia'),
-    path('readBiomaCerrado', readBiomaCerrado, name='readBiomaCerrado'),
-    path('readBiomaCaatinga', readBiomaCaatinga, name='readBiomaCaatinga'),
-    path('readBiomaPampa', readBiomaPampa, name='readBiomaPampa'),
-    path('readBiomaPantanal', readBiomaPantanal, name='readBiomaPantanal'),
-    path('readBiomaMataAtlantica', readBiomaMataAtlantica, name='readBiomaMataAtlantica'),
+    path('listar_cerrado', listar_cerrado, name='listar_cerrado'),
+    path('listar_caatinga', listar_caatinga, name='listar_caatinga'),
+    path('listar_pampa', listar_pampa, name='listar_pampa'),
+    path('listar_pantanal', listar_pantanal, name='listar_pantanal'),
+    path('listar_atlantica', listar_atlantica, name='listar_atlantica'),
     path('delete_amazonia/<int:id>', delete_amazonia, name='delete_amazonia'),
-    path('deleteBiomaCerrado/<int:id>', deleteBiomaCerrado, name='deleteBiomaCerrado'),
-    path('deleteBiomaCaatinga/<int:id>', deleteBiomaCaatinga, name='deleteBiomaCaatinga'),
-    path('deleteBiomaPampa/<int:id>', deleteBiomaPampa, name='deleteBiomaPampa'),
-    path('deleteBiomaPantanal/<int:id>', deleteBiomaPantanal, name='deleteBiomaPantanal'),
-    path('deleteBiomaMataAtlantica/<int:id>', deleteBiomaMataAtlantica, name='deleteBiomaMataAtlantica'),
+    path('delete_cerrado/<int:id>', delete_cerrado, name='delete_cerrado'),
+    path('delete_caatinga/<int:id>', delete_caatinga, name='delete_caatinga'),
+    path('delete_pampa/<int:id>', delete_pampa, name='delete_pampa'),
+    path('delete_pantanal/<int:id>', delete_pantanal, name='delete_pantanal'),
+    path('delete_atlantica/<int:id>', delete_atlantica, name='delete_atlantica'),
     path('api/', include(router.urls)),
 ]
